5cbed3b3e1f76108ba91afd7640c83439b43e16c
nngw <thanushiyan@hotmail.co.uk>
2023-05-17 12:14:46 +0100

    add index.jsx file to pages

--- music_app/src/main.jsx
+++ music_app/src/main.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Album from './Albums.jsx'
-import Form from './Form.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+
+// import App from './App.jsx'
+// import Album from './Albums.jsx'
+// import Form from './Form.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* <App />
     <Album/>
-    <Form/>
+    <Form/> */}
   </React.StrictMode>,
 )
