73d3190b36a05e2fcf4b2e9e30b1e0feeb8ec943
nngw <thanushiyan@hotmail.co.uk>
2023-05-18 12:08:55 +0100

    add fetch call

--- music_app/src/pages/Songs/index.jsx
+++ music_app/src/pages/Songs/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
 import LikeButton from '../../componants/LikeButton'
+import axios from 'axios'
 
 function Songs() {
-  // const [likes, setCount] = useState(0)
+  async function getArtistDetails () {
+    const res = await axios.get('https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=BringMethehorizon&api_key=20e0ac2d870553ac17505122660cd87b&format=json')
+
+    console.log(res.data)
+    
+  }
+  getArtistDetails()
 
   return (
     <>
       <h3>songs</h3>
-      <ul>
+      {/* <ul>
         <li className='liContainer'>
           <div className='flexContainer'>
            <LikeButton />
@@ -17,7 +24,7 @@ function Songs() {
               <h4>Album title: Sepriternal</h4>
               <h4>Rlease year: 2013</h4>
             </div>
-            {/* <h4 className='likeCount'>Likes: {likes}</h4> */}
+            <h4 className='likeCount'>Likes: {likes}</h4>
           </div>
         </li>
         <li className='liContainer'>
@@ -30,10 +37,10 @@ function Songs() {
               <h4>Album title: That's The Spirit</h4>
               <h4>Rlease year: 2015</h4>
             </div>
-            {/* <h4 className='likeCount'>Likes: {likes}</h4> */}
+            <h4 className='likeCount'>Likes: {likes}</h4>
           </div>
         </li>
-      </ul>
+      </ul> */}
     </>
   )
 }
